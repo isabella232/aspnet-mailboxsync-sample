1a4ba803eb5f5ba94be666ffe6ffcf5273333de1
Charles Wahome <thewahome@users.noreply.github.com>
2018-08-01 09:56:32 +0300

    initial commit

---
+++ MailboxSync/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿/* 
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+*  See LICENSE in the source repository root for complete license information. 
+*/
+
+using System.Web.Mvc;
+
+namespace MailboxSync.Controllers
+{
+
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
